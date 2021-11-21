9a014d22b6c0a4841f546c3b2557e40f88d7f177
Ravi Prakash <41155449+raviprakashdev@users.noreply.github.com>
2021-11-22 01:17:37 +0530

    state

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,17 +1,34 @@
 import React from "react";
 
 class AddContact extends React.Component {
+
+    state = {
+        name:"",
+        email:""
+    }
+    add = (e) => {
+        e.preventDefault();
+        if (this.state.name === "" && this.state.email === "") {
+            alert("All the fields are required");
+            return;
+        }
+        console.log(this.state)
+        this.props.addContactHandler(this.state)
+    }
+
     render() {
         return (
             <div className="ui main">
                 <h2>Add Contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
                         <input
                             type="text"
                             name="name"
                             placeholder="Name"
+                            value={this.state.name}
+                            onChange={(e) => this.setState({ name: e.target.value })}
                         ></input>
                     </div>
                     <div className="field">
@@ -20,9 +37,11 @@ class AddContact extends React.Component {
                             type="text"
                             name="email"
                             placeholder="Eamil"
+                            value={this.state.email}
+                            onChange={(e) => this.setState({ email: e.target.value })}
                         ></input>
                         <div>
-                            <button className="ui button blue">Add</button>
+                            <button className="ui button blue" >Add</button>
                         </div>
                     </div>
                 </form>
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React,useState} from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -6,22 +6,28 @@ import ContactList from "./ContactList";
 
 function App() {
 
-    const contacts = [
-        {
-            id: "1",
-            name: "Ravi Prakash",
-            email: "raviprakash01a@gmail.com",
-        },
-        {
-            id: "2",
-            name: "Ravi Prakash2",
-            email: "raviprakashogs@gmail.com",
-        },
-    ];
+    const [contacts, setContacts] = useState([]);
+    
+    const addContactHandler = (contact) => {
+        console.log(contact);
+    }
+
+    // const contacts = [
+    //     {
+    //         id: "1",
+    //         name: "Ravi Prakash",
+    //         email: "raviprakash01a@gmail.com",
+    //     },
+    //     {
+    //         id: "2",
+    //         name: "Ravi Prakash2",
+    //         email: "raviprakashogs@gmail.com",
+    //     },
+    // ];
     return (
         <div className="ui container">
             <Header />
-            <AddContact />
+            <AddContact addContactHandler = {addContactHandler} />
             <ContactList contacts={contacts} />
         </div>
     );
